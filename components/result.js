dc57750e9db8833a67024b0af1643781ae8566b4
Geir Gåsodden <geir.gasodden@pythonia.no>
2021-08-03 17:40:54 +0200

    Adds reset button on results

--- components/result.js
+++ components/result.js
@@ -1,8 +1,24 @@
-function Result ({ result }) {
+function Result ({ result, setResult }) {
+  const handleReset = () => setResult(false)
   if (!result) return null
 
   return (
-    <>{JSON.stringify(result, null, 2)}</>
+    <div>
+      <div>
+      {JSON.stringify(result, null, 2)}
+    </div>
+    <div className="pt-5">
+      <div className="flex justify-end">
+        <button
+          type="button"
+          className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          onClick={handleReset}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+    </div>
   )
 }
 
